5e08c02d39f1508289b3df9bf93d0febbcda0641
axy888 <htd104104@gmail.com>
2024-03-27 19:00:15 +0700

    thêm bus

---
+++ src/main/java/BUS/ThietBiBUS.java
@@ -0,0 +1,44 @@
+package BUS;
+
+import java.util.ArrayList;
+import DAO.ThietBiDAO;
+import DTO.ThietBi;
+
+public class ThietBiBUS {
+    static ArrayList<ThietBi> listTB;
+    ThietBiDAO dao;
+
+    public ArrayList<ThietBi> getData()
+    {
+        dao= new ThietBiDAO();
+        listTB=dao.getData();
+        return listTB;
+    }
+
+    public void addThietBi(ThietBi tb)
+    {
+        dao=new ThietBiDAO();
+        dao.addThietBi(tb);
+    }
+
+    public Boolean delThietBi(int ID)
+    {
+        dao= new ThietBiDAO();
+        if( dao.delThietBi(ID)) return true;
+        return false;
+        
+    }
+
+    public Boolean updateThietBi(int ID,ThietBi tb)
+    {
+        dao= new ThietBiDAO();
+        if(dao.updateThietBi(ID, tb))return true;
+        return false;
+    }
+
+    public ThietBi getThanhVienByID(int ID)
+    {
+        dao= new ThietBiDAO();
+        return dao.geThietBiByID(ID);
+    }
+}
---
+++ src/main/java/BUS/ThongTinSDBUS.java
@@ -0,0 +1,44 @@
+package BUS;
+
+import java.util.ArrayList;
+import DAO.ThongTinSDDAO;
+import DTO.ThongTinSD;
+
+public class ThongTinSDBUS {
+    static ArrayList<ThongTinSD> listTT;
+    ThongTinSDDAO dao;
+
+    public ArrayList<ThongTinSD> getData()
+    {
+        dao= new ThongTinSDDAO();
+        listTT=dao.getData();
+        return listTT;
+    }
+
+    public void addTTSD(ThongTinSD tt)
+    {
+        dao=new ThongTinSDDAO();
+        dao.addTTSD(tt);
+    }
+
+    public Boolean delTTSD(int ID)
+    {
+        dao= new ThongTinSDDAO();
+        if( dao.delTTSD(ID)) return true;
+        return false;
+        
+    }
+
+    public Boolean updateTTSD(int ID,ThongTinSD tt)
+    {
+        dao= new ThongTinSDDAO();
+        if(dao.updateTTSD(ID, tt))return true;
+        return false;
+    }
+
+    public ThongTinSD getTTSDByID(int ID)
+    {
+        dao= new ThongTinSDDAO();
+        return dao.geThongTinSDByID(ID); 
+    }
+}
